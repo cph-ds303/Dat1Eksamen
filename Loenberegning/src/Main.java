b8de68c54621d9e286a9832f63bc0b2b9d10c98f
cph-ds303 <cph-ds303@cphbusiness.dk>
2025-06-02 15:11:10 +0200

    update

--- Loenberegning/src/Main.java
+++ Loenberegning/src/Main.java
@@ -1,14 +1,13 @@
+import java.util.ArrayList;
 
-
-    public class Main {
+public class Main {
     public static void main(String[] args) {
 
 
-        Employee employee1 = new HourlyEmployee("Tumi", 50, 226);
-        Employee employee2 = new CommissionEmployee("Aoife", 200, 1000, 0.5);
-        Employee employee3 = new MonthlyEmployee("Dino", 30000);
-
-        Employee[] employees = {employee1, employee2, employee3};
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.add(new HourlyEmployee("Tumi", 50, 226));
+        employees.add(new CommissionEmployee("Aoife", 200, 1000, 0.5));
+        employees.add(new MonthlyEmployee("Dino", 30000));
 
         for(Employee i : employees)
             System.out.println(i.getEmployeeName() + " " + i.calculateSalary());
